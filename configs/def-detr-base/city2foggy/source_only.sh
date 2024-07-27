39d149d27b8ee6f34aa4e659570364409c4de67b
wst2001 <weisitong@pku.edu.cn>
2024-07-27 11:58:08 +0800

    1

--- configs/def-detr-base/city2foggy/source_only.sh
+++ configs/def-detr-base/city2foggy/source_only.sh
@@ -3,7 +3,7 @@ BATCH_SIZE=8
 DATA_ROOT=/scratch/generalvision/zhaozijing/data
 OUTPUT_DIR=./outputs/def-detr-base/city2foggy/source_only
 
-CUDA_VISIBLE_DEVICES=1,2 OMP_NUM_THREADS=4 torchrun \
+CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=4 torchrun \
 --rdzv_endpoint localhost:26500 \
 --nproc_per_node=${N_GPUS} \
 main.py \
